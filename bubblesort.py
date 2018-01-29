fcd379e2cc54bf1d882d1b852ca5975ff709ad90
lpjacob <32037184+lpjacob@users.noreply.github.com>
2018-01-29 19:06:34 +0000

    Update bubblesort.py
    I have put in to a function to promote re-use (modular code is also easier to debug). Comments added and and put in a test list of strings

--- bubblesort.py
+++ bubblesort.py
@@ -1,15 +1,16 @@
-array = [10, 3, 123, 7, 3, 2, 5, 6, 7]
+def my_bubble_sort(someList):#funtion with list as a prameter
+    swapped = True
+    while swapped == True: #check if a swap has been made, if true repeat
+        swapped = False #reset swapped value to false
+        for i in range(len(someList)-1): #iterate over list until one from the end
+            if someList[i] > someList[i+1]:#the greater sign will sort in ascending order
+                temp = someList[i] #store the value to be replaced in a temporary variable
+                someList[i] = someList[i+1] #swap the value to the right with the current value
+                someList[i + 1] = temp #replace value on right with temp value
+                print(someList)# print to watch the values moving - optional
+                swapped = True #a swap has been made so set to true
+    return someList
 
-swapped = True
-while swapped:
-    swapped = False
-    i = 0
-    for e in array:
-        i += 1
-        if i < len(array):
-            if array[i] < e:
-                array[i - 1] = array[i]
-                array[i] = e
-                swapped = True
-
-print(array)
+currentList =["zebra", "mouse", "cat", "dog"]
+print (currentList)
+sortedList = my_bubble_sort(currentList)
